4ff172135e918991867b5310761b481a89cf784f
Mudathir M Muhammad <khalifasitemsn@gmail.com>
2024-06-01 14:42:09 +0100

    admin home updated

--- Management/adminLogin.php
+++ Management/adminLogin.php
@@ -2,133 +2,94 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link
-      href="https://fonts.googleapis.com/css?family=Courgette"
-      rel="stylesheet"
-    />
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <script src="js/bootstrap.min.js"></script>
-    <style>
-     body
-      {
-        background-image: url('home1.jpg');
-        background-repeat:no-repeat;
-      }
-      
-
-
-#div_login{
-    border: 1px solid gray;
-    border-radius: 3px;
-    width: 500px;
-    height: 390px;
-    box-shadow: 0px 2px 2px 0px  gray;
-    margin: 0 auto;
-     background-color:black;
-        opacity: 0.7;
-}
-
-#div_login h1{
-    margin-top: 0px;
-    font-weight: normal;
-    padding: 10px;
-    background-color: cornflowerblue;
-    color: white;
-    font-family: sans-serif;
-}
-
-#div_login div{
-    clear: both;
-    margin-top: 10px;
-    padding: 5px;
-}
-
-#div_login .textbox{
-    width: 96%;
-    padding: 7px;
-}
-
-    </style>
-
-    <title>Login | Admin Panel</title>
+    <title>Login | travel_management</title>
     
+    <link href="css/admin.css" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap-select.css" rel="stylesheet">
+    <link href="css/bootstrap-datetimepicker.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400|Raleway:100,300,400,500|Roboto:100,400,500,700" rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/bootstrap-select.js"></script>
+    <script src="js/bootstrap-dropdown.js"></script>
+    <script src="js/jquery-2.1.1.min.js"></script>
+    <script src="js/moment-with-locales.js"></script>
+    <script src="js/bootstrap-datetimepicker.js"></script>
 </head>
-
-<body> 
-<center> 
-
-<h1 class="text-center" style="font-size: 60px;
-        font-family: Courgette;
-        text:black">Admin Panel</h1>
-<br>
-<br>
-<br>
-<br>
-<div class="container">
-    <form method="post" action="">
-        <div class="align-center" id="div_login">
-            <h1 class="text-center">Login</h1>
-            <div>
-                <h3 class="text-white">Username:</h3>
-                <input type="text" class="textbox " id="txt_uname" name="username" placeholder="Enter Username" />
+<body>
+    <div class="container-fluid">
+        <div class="login">
+            <div class="col-sm-12">
+                <div class="heading text-center">
+                    Login
+                </div>
             </div>
-            <div>
-            <h3 class="text-white">Password:</h3>
-                <input type="password" class="textbox" id="txt_uname" name="password" placeholder="Enter Password"/>
-            </div>
-            <br>
-            <div>
-               <h3><input class="btn-success" type="submit" value="Login" name="but_submit" id="but_submit" /></h3>
+            <div class="col-sm-12">
+                <div class="containerBox">
+                    <form action="" method="POST">
+                        <label for="username">Username:</label>
+                        <input type="text" class="input" name="username" placeholder="Enter username here" required>
+                        <label for="password">Password:</label>
+                        <input type="password" class="input" name="password" placeholder="Enter password here" required>
+                        <div class="col-sm-12 text-center">
+                            <input type="submit" class="button" name="but_submit" value="Login">
+                        </div>
+                        <!-- <a href="forgotPassword.php"><p class="dots">Forgot Password?</p></a> -->
+                    </form>
+                    <div class="col-sm-12 text-center">
+                        <div class="signupPrompt">
+                            <!-- New user? <a href="signup.php">Sign Up</a> instead. -->
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-    </form>
-</div>
-</center>
+    </div>
+    <?php
+    session_start();
 
-<?php
-session_start();
+    $servername = "localhost";
+    $usernameConn = "root";
+    $passwordConn = "";
+    $dbname = "tourism_db";
 
-        $servername = "localhost";
-		$usernameConn = "root";
-		$passwordConn = "";
-		$dbname = "tourism_db";
-		
-		// Creating a connection to tourism_db MySQL database
-		$conn = mysqli_connect($servername, $usernameConn, $passwordConn, $dbname);
-		
-		// Checking if we've successfully connected to the database
-		if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
-}
+    // Creating a connection to tourism_db MySQL database
+    $conn = mysqli_connect($servername, $usernameConn, $passwordConn, $dbname);
 
-if(isset($_POST['but_submit'])){
-
-    $username = mysqli_real_escape_striqng($conn,$_POST['username']);
-    $password = mysqli_real_escape_string($conn,$_POST['password']);
-
-    if ($username != "" && $password != ""){
-
-        $sql_query = "SELECT count(*) as cntUser FROM `admin` WHERE username='".$username."' AND password='".$password."'";
-        $result = mysqli_query($conn,$sql_query);
-        $row = mysqli_fetch_array($result);
-
-        $count = $row['cntUser'];
-
-        if($count > 0){
-            $_SESSION['username'] = $username;
-            header('Location: Home.php');
-        }else{
-            echo "<h4 class='text-center bg-danger' style='font-weight:bold';>Invalid username and password</h4>";
-        }
-
+    // Checking if we've successfully connected to the database
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
     }
 
-}
-?>
+    if (isset($_POST['but_submit'])) {
+        $username = mysqli_real_escape_string($conn, $_POST['username']);
+        $password = mysqli_real_escape_string($conn, $_POST['password']);
 
+        if ($username != "" && $password != "") {
+            $sql_query = "SELECT password FROM admin WHERE username='$username'";
+            $result = mysqli_query($conn, $sql_query);
 
+            if ($result && mysqli_num_rows($result) > 0) {
+                $row = mysqli_fetch_assoc($result);
+                
+                if ($password === $row['password']) {
+                    $_SESSION['username'] = $username;
+                    header('Location: Home.php');
+                    exit(); // Ensure no further code is executed after redirect
+                } else {
+                    echo "<h4 class='text-center bg-danger' style='font-weight:bold;'>Invalid username and password</h4>";
+                }
+            } else {
+                echo "<h4 class='text-center bg-danger' style='font-weight:bold;'>Invalid username and password</h4>";
+            }
+        }
+    }
 
+    mysqli_close($conn);
+    ?>
 </body>
 </html>
